56e166d9ef5eeb2e692bb6a2173d6839af856244
mason <zyabg28@gmail.com>
2026-09-10 10:58:09 -0600

    LED lab

---
+++ light.py
@@ -0,0 +1,14 @@
+from gpiozero import LED
+from time import sleep
+
+red = LED(18)
+
+while True:
+    red.on()
+    sleep(1)
+    red.off()
+    sleep(1)
+    red.on()
+    sleep(0.1)
+    red.off()
+    sleep(1)
